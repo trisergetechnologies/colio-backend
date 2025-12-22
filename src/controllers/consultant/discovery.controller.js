2680fc40e8427a5a729b3901d377fcd76aa0085a
Ojas kumar mishra <Ojasmishra525@gmail.com>
2025-12-23 01:52:59 +0530

    error resolved

--- src/controllers/consultant/discovery.controller.js
+++ src/controllers/consultant/discovery.controller.js
@@ -248,7 +248,7 @@ export const quickConnect = async (req, res) => {
  */
 export const getConsultantDetails = async (req, res) => {
   try {
-    const { id: consultant  Id } = req.params;
+    const { id: consultantId } = req.params;
     const customerId = req.user.userId;
     
     // Find consultant
