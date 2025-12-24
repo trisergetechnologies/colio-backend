5e3c7b1fcc324e3b77242efae5ba9bc9859285eb
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-12-24 16:34:39 +0530

    notfication fixes 2

--- src/routes/user/notificationRoutes.js
+++ src/routes/user/notificationRoutes.js
@@ -1,7 +1,7 @@
 // routes/notificationRoutes.js
 import express from 'express';
-// import User from '../models/User.js';
-// import firebaseService from '../services/firebaseService.js';
+import User from '../../models/User.js';
+import firebaseService from '../../services/firebaseService.js';
 
 const router = express.Router();
 
